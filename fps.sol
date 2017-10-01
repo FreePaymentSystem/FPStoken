fa63258a0cfc41ec798b059a86a91b9fd9d9ccfb
trigger86 <olegg86reg@gmail.com>
2017-10-02 01:35:39 +0500

    Update fps.sol

--- fps.sol
+++ fps.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.11;
 /**
  * @title SafeMath
  * @dev Math operations with safety checks that throw on error
+ * КОД СЫРОЙ, ТЕСТИРОВАТЬ НЕОБХОДИМО НА testnet.
  */
 contract SafeMath {
 
